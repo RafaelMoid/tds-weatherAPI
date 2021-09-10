99804c7dfab19b278fbb593a1be7b043e5da3b34
Rafael Varela <rafaelvrcr@gmail.com>
2021-09-10 12:52:53 -0300

    (CSS Congir) & (Upgrade Routes)

--- src/App.js
+++ src/App.js
@@ -8,21 +8,46 @@ import Vancouver from './pages/Vancouver(CA)';
 import Yakutsk from './pages/Yakutsk(RU)';
 import {Route, Link} from 'react-router-dom';
 import NavBar from './NavBar';
-import axios from './axios';
+import styled from 'styled-components';
+
+const WW = styled.img `
+width: 80px;
+height: 80px;
+`
+
+const Title = styled.h3 `
+height: 0px;
+`
+
+const SubTitle = styled.p `
+width: 140px;
+height: 30px;
+font-size: 20px;
+`
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to Rafael's weather API service</h1>
+       
+              
+        <Title>WEATHER</Title>
+        <SubTitle>select a city</SubTitle>
+        <WW className="WW" src="\img\worldwidewhite.png" ></WW>
         <NavBar />
-        <Route exact path="/dallol" component={Dallol} />
-        <Route exact path="/fairbanks" component={Fairbanks} />
-        <Route exact path="/londres" component={Londres} />
-        <Route exact path="/recife" component={Recife} />
-        <Route exact path="/vancouver" component={Vancouver} />
-        <Route exact path="/yakutsk" component={Yakutsk} />
-        <Route exact path="/" />
+        <ul className="Ul">
+          <Route exact path="/dallol" component={Dallol} />
+          <Route exact path="/fairbanks" component={Fairbanks} />
+          <Route exact path="/londres" component={Londres} />
+        </ul>  
+        <ul className="Ul">
+          <Route exact path="/recife" component={Recife} />
+          <Route exact path="/vancouver" component={Vancouver} />
+          <Route exact path="/yakutsk" component={Yakutsk} />
+          </ul>
+        
       </header>
     </div>
   );
--- src/pages/Dallol(NG).js
+++ src/pages/Dallol(NG).js
@@ -1,29 +1,39 @@
 import React,{Fragment} from "react";
 import styled from 'styled-components';
+import {Route} from 'react-router-dom'
 
 const WeatherLogo =styled.img`
+width: 140px;
+height: 140px;
+margin: 40px auto;
 
 `
+
 
 function Dallol(){
     return(
         <Fragment>
-            <h3 className="title">Dallol(NG)</h3>
-            <br/>
-            <p>clima atual</p>
-            <p>temperatura</p>
-            <ul className="listaHora">
-                <li>dawn</li>
-                <li>morning</li>
-                <li>afternoon</li>
-                <li>night</li>
-            </ul>
-            <ul className="listaDados">
-                <li>wind speed</li>
-                <li>sunrise</li>
-                <li>sunset</li>
-                <li>humidity</li>
-            </ul>
+            
+                <h3 className="title">Dallol</h3>
+                <br/>
+                <p>clima atual</p>
+                <p>temperatura</p>
+                <WeatherLogo src="/icons/perfect-day.svg"/>
+                <ul className="listaHora">
+                    <li>dawn</li>
+                    <li>morning</li>
+                    <li>afternoon</li>
+                    <li>night</li>
+                </ul>
+                <ul className="listaDados">
+                    <li>wind speed</li>
+                    <li>sunrise</li>
+                    <li>sunset</li>
+                    <li>humidity</li>
+                </ul>
+                  
+          <Route exact path="/" />
+            
         </Fragment>
     );
 }
