49db363706e75ebb6f9bbed6840680e5934b4654
Rafael Varela <rafaelvrcr@gmail.com>
2021-09-10 05:24:12 -0300

    (Config) Routes & Sleep 'cause i'm tired

--- src/App.js
+++ src/App.js
@@ -6,18 +6,19 @@ import Londres from './pages/Londres(GB).js';
 import Recife from './pages/Recife(BR).js';
 import Vancouver from './pages/Vancouver(CA)';
 import Yakutsk from './pages/Yakutsk(RU)';
+import {Route, Link} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Welcome to Rafael's weather API service</h1>
-        <Dallol />
-        <Fairbanks />
-        <Londres />
-        <Recife />
-        <Vancouver />
-        <Yakutsk/>
+        <Route exact path="/dallol" component={Dallol} />
+        <Route exact path="/fairbanks" component={Fairbanks} />
+        <Route exact path="/londres" component={Londres} />
+        <Route exact path="/recife" component={Recife} />
+        <Route exact path="/vancouver" component={Vancouver} />
+        <Route exact path="/yakutsk" component={Yakutsk} />
       </header>
     </div>
   );
